872f8d1650d295029821c67a107b1720c0c69975
cloudXA <zming794@gmail.com>
2020-08-30 10:11:42 +0800

    Updates

---
+++ precache-manifest.f26c4aa7c7c35cea24009405a5fecd18.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea078221edb7c6458f78a340f55c2f90",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "e5695b73568feef2e3e7",
+    "url": "/react-todo/static/css/main.4d15eff5.chunk.css"
+  },
+  {
+    "revision": "d2d2d0198f4e7dacfa83",
+    "url": "/react-todo/static/js/2.605aba26.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-todo/static/js/2.605aba26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5695b73568feef2e3e7",
+    "url": "/react-todo/static/js/main.eabebb67.chunk.js"
+  },
+  {
+    "revision": "76e3949e20e9ece5359b",
+    "url": "/react-todo/static/js/runtime-main.4d3e8540.js"
+  }
+]);
